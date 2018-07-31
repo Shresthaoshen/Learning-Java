8db81541aa5cde43c97e7779c04dfbd619626a9b
Oshen Shrestha <shresthao@mymail.vcu.edu>
2018-07-31 17:33:18 -0400

    part 1 of learning java

---
+++ Dog.java
@@ -0,0 +1,28 @@
+public class Dog{
+
+
+String breed;
+int age;
+String color;
+
+public static void main(String[] args){
+
+
+}
+
+
+void barking(){
+
+}
+
+void hungry(){
+
+}
+
+void sleeping(){
+
+
+}
+
+
+}
